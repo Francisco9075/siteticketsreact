9ff5f62a5972a2b633b8a22e9a42cad0134a7508
Francisco9075 <chiquitodaniel008@gmail.com>
2025-05-28 20:14:30 +0100

    login feito

--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -1,3 +1,4 @@
+
 import EcommerceMetrics from "../../components/ecommerce/EcommerceMetrics";
 import MonthlySalesChart from "../../components/ecommerce/MonthlySalesChart";
 import StatisticsChart from "../../components/ecommerce/StatisticsChart";
@@ -6,7 +7,10 @@ import RecentOrders from "../../components/ecommerce/RecentOrders";
 import DemographicCard from "../../components/ecommerce/DemographicCard";
 import PageMeta from "../../components/common/PageMeta";
 
+
+
 export default function Home() {
+  
   return (
     <>
       <PageMeta
@@ -16,22 +20,17 @@ export default function Home() {
       <div className="grid grid-cols-12 gap-4 md:gap-6">
         <div className="col-span-12 space-y-6 xl:col-span-7">
           <EcommerceMetrics />
-
           <MonthlySalesChart />
         </div>
-
         <div className="col-span-12 xl:col-span-5">
           <MonthlyTarget />
         </div>
-
         <div className="col-span-12">
           <StatisticsChart />
         </div>
-
         <div className="col-span-12 xl:col-span-5">
           <DemographicCard />
         </div>
-
         <div className="col-span-12 xl:col-span-7">
           <RecentOrders />
         </div>
