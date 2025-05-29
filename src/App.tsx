a98404a3af5c950c261c2c85fa03f81cf5d3de37
GHP <goncalopaulino25@gmail.com>
2025-05-30 00:51:42 +0100

    Adição da rota logout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import SignIn from "./pages/AuthPages/SignIn";
 import SignUp from "./pages/AuthPages/SignUp";
+import Logout from "./pages/components/auth/Logout";
 import NotFound from "./pages/OtherPage/NotFound";
 import UserProfiles from "./pages/UserProfiles";
 import Videos from "./pages/UiElements/Videos";
@@ -64,6 +65,7 @@ export default function App() {
           {/* Auth Layout */}
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/logout" element={<Logout />} />
 
           {/* Fallback Route */}
           <Route path="*" element={<NotFound />} />
